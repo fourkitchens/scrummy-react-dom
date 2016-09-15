47c0b487633544d16bbebec909154f24ac391479
Luke Herrington <lherrington100@gmail.com>
2016-09-14 21:39:53 -0700

    Add Client component.

---
+++ src/components/Client/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Client.scss';
+
+const Client = ({ nickname, vote }) => (
+  <div className={`client ${vote ? 'voted' : ''}`}>
+    <div className="back">
+      <div className="nickname">{nickname}</div>
+    </div>
+    <div className="front">
+      <div className="nickname">{nickname}</div>
+      <div className="vote-wrap"><span className="vote">{vote}</span></div>
+    </div>
+  </div>
+);
+
+Client.propTypes = {
+  nickname: React.PropTypes.string,
+  vote: React.PropTypes.string,
+};
+
+export default Client;
