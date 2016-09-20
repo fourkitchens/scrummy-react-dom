482735279f6b6c0d3ba8841e422f3190a394632c
Luke Herrington <lherrington100@gmail.com>
2016-09-20 14:10:24 -0700

    Fix phantom lint issues 👻.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
 import { configure } from '@kadira/storybook';
 // Get all stories from components.
-const req = require.context('../src/components', true, /stories.js/)
+const req = require.context('../src/components', true, /stories.js/);
 configure(() => req.keys().forEach((filename) => req(filename)), module);
--- src/components/VotingResult/index.js
+++ src/components/VotingResult/index.js
@@ -6,17 +6,17 @@ import './VotingResult.scss';
 const VotingResult = ({ nickname, gameLinkToggled, onReveal, onReset, onGameLinkToggle }) => (
   <section id="votingResult">
     <div id="votingActions">
-      <input type="button" id="btnReveal" value="Reveal" onClick={onReveal}></input>
-      <input type="button" id="btnReset" value="Reset" onClick={onReset}></input>
+      <input type="button" id="btnReveal" value="Reveal" onClick={onReveal} />
+      <input type="button" id="btnReset" value="Reset" onClick={onReset} />
       <input
         type="button"
         id="btnLink"
         value="Game Link"
         className={classNames({ active: gameLinkToggled })}
         onClick={onGameLinkToggle}
-      ></input>
+      />
       <div id="gameLink" className={classNames({ open: gameLinkToggled })}>
-        <input type="text" id="txtUrl" value={window.location.href} readOnly></input>
+        <input type="text" id="txtUrl" value={window.location.href} readOnly />
       </div>
     </div>
     <Client nickname={nickname} />
